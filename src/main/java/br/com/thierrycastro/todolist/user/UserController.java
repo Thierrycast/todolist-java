110bdf756d84cfb41f55561185ae293e37f70f30
Thierry Castro <thierrycastro555@gmail.com>
2023-10-16 19:38:23 -0300

    base version todo list

--- src/main/java/br/com/thierrycastro/todolist/user/UserController.java
+++ src/main/java/br/com/thierrycastro/todolist/user/UserController.java
@@ -1,17 +1,35 @@
 package br.com.thierrycastro.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
+    @Autowired
+    private IUserRepository UserRepository;
 
-        System.out.println(userModel.getName());
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.UserRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(400).body("Usuário já existe");
+        }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
+        var userCreated = this.UserRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
